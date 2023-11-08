139ffd405154983e58927767488bad8274210154
janinquisitor <jancarlos0195@gmail.com>
2023-11-08 19:45:42 -0400

    Testing out Javalin, routing and modified the pom.xml file.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,41 @@
 package org.example;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import io.javalin.Javalin;
+import io.javalin.json.JsonMapper;
+import org.jetbrains.annotations.NotNull;
+
+import java.lang.reflect.Type;
+
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Gson gson = new GsonBuilder().create();
+
+        JsonMapper gsonMapper = new JsonMapper() {
+            @Override
+            public String toJsonString(@NotNull Object obj, @NotNull Type type) {
+                return gson.toJson(obj, type);
+            }
+
+            @Override
+            public <T> T fromJsonString(@NotNull String json, @NotNull Type targetType) {
+                return gson.fromJson(json, targetType);
+            }
+        };
+        var app = Javalin.create(config -> {
+                    config.jsonMapper(gsonMapper);
+                })
+                .get("/", ctx -> ctx.result("Hello World"))
+                .start(7070);
+
+
+        Response response = new Response("This is a JSON Object", 12);
+
+        app.get("/testing", ctx -> ctx.json(response));
+        app.get("/hello/{name}", ctx -> ctx.result("Hello " + ctx.pathParam("name")));
     }
 }
---
+++ src/main/java/org/example/Response.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Response {
+
+    String message;
+
+    int data;
+
+    public Response(String message, int data) {
+        this.message = message;
+        this.data = data;
+    }
+}
